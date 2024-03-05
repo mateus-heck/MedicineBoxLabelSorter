5e532f9b254cb14d660d5151bbe2e258b9f243ed
Claudio Honorio da Silva Junior <83031994+ClaudioSilvaJ@users.noreply.github.com>
2024-03-05 19:05:19 -0300

    Update filtros.py

--- filtros.py
+++ filtros.py
@@ -14,7 +14,7 @@ sharpened_image = sharpen(imagem)
 _, binary_image = cv2.threshold(sharpened_image, 1, 255, cv2.THRESH_BINARY)
 
 thresh = cv2.threshold(binary_image, 115, 255, cv2.THRESH_BINARY_INV)[1]
-kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3,2))
+kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3,2)) #Mexer aqui
 dilate_2 = cv2.dilate(thresh, kernel, iterations=1)
 final = cv2.threshold(dilate_2, 115, 255, cv2.THRESH_BINARY_INV)[1]
 
