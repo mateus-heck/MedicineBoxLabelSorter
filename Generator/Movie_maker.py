2869c77d051955a8d7dae6e8a854fa5b5ea925d5
Mateus Heck Soares <mateus.soares@biopark.com.br>
2024-05-06 16:56:48 -0300

    Conclude movie_maker.py function

--- Generator/Movie_maker.py
+++ Generator/Movie_maker.py
@@ -14,8 +14,52 @@ imagens_boas = [cv2.imread("MedicineBoxLabelSorter/Generator/Good/"+imagem) for 
 imagens_ruins = [cv2.imread("MedicineBoxLabelSorter/Generator/Bad/"+imagem) for imagem in os.listdir('MedicineBoxLabelSorter/Generator/Bad')]
 
 # Criar clipes de vídeo para imagens boas
-clips_boas = [(mp.ImageClip(img).set_duration(0.1)) for img in imagens_boas]
+clips_boas = [(mp.ImageClip(img).set_duration(0.2)) for img in imagens_boas]
 
-concat_clip = [mp.CompositeVideoClip([clip.fx( mp.transfx.slide_out, 0.1, 'left')]) for clip in clips_boas]
-concat_clip = mp.concatenate_videoclips(concat_clip)
+## Com os clipes criados e reservados em memória, vamos criar o slideshow
+#Variaveis do clipe
+duracao_efeito = 0.1
+duracao_clipe = 0.1
+# Começamos com a imagem estática inicial e terminamos com a final da mesma forma
+first_clip = mp.CompositeVideoClip(
+    [
+        clips_boas[0]
+        .set_pos("center")
+        .fx(mp.transfx.slide_out, duration=duracao_efeito, side="left")
+    ]
+).set_start((duracao_clipe - duracao_efeito) * 0)
+
+# Para o último video começamos da direita para a esquerda
+last_clip = (
+    mp.CompositeVideoClip(
+        [
+            clips_boas[-1]
+            .set_pos("center")
+            .fx(mp.transfx.slide_in, duration=duracao_efeito, side="right")
+        ]
+    )
+    .set_start((duracao_clipe - duracao_efeito) * (len(clips_boas) - 1))
+    .fx(mp.transfx.slide_out, duration=duracao_efeito, side="left")
+)
+videos = (
+    [first_clip]
+    # Para todos os clipes no meio aplicamos o slide in do último com slide out para o próximo
+    + [
+        (
+            mp.CompositeVideoClip(
+                [
+                    clip.set_pos("center").fx(
+                        mp.transfx.slide_in, duration=duracao_efeito, side="right"
+                    )
+                ]
+            )
+            .set_start((duracao_clipe - duracao_efeito) * idx)
+            .fx(mp.transfx.slide_out, duration=duracao_efeito, side="left")
+        )
+        for idx, clip in enumerate(clips_boas[1:-1], start=1)
+    ]
+    + [last_clip]
+)
+
+concat_clip = mp.concatenate_videoclips(videos)
 concat_clip.write_videofile("MedicineBoxLabelSorter/Generator/videos/Final.mp4", fps=24)
