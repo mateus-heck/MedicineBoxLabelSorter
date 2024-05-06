f7284b494fc811c932bffbf506f4b8afad30d753
Mateus Heck Soares <mateus.soares@biopark.com.br>
2024-05-06 16:34:15 -0300

    Saving only the final video with slide-out effect

--- Generator/Movie_maker.py
+++ Generator/Movie_maker.py
@@ -1,63 +1,21 @@
 import cv2
 import moviepy.editor as mp
 import os
-import moviepy.video.compositing.transitions as transfx
-from moviepy.decorators import requires_duration
-from moviepy.video.compositing.CompositeVideoClip import CompositeVideoClip
-from moviepy.video.VideoClip import ImageClip
+
 # Definir as variáveis
 n_imagens_boas = 10  # Número de imagens boas
 n_imagens_ruins = 10  # Número de imagens ruins
 fps = 24  # Taxa de quadros por segundo
-duracao_video = 5  # Duração do vídeo em segundos
-
+duracao_video_total = 20  # Duração do vídeo em segundos
+duracao_video_indv = 1 # Duração de cada clipe
 
 # Carregar as imagens
-imagens_boas = [cv2.imread("MedicineBoxLabelSorter/Good/"+imagem) for imagem in os.listdir('MedicineBoxLabelSorter/Good')]
-imagens_ruins = [cv2.imread("MedicineBoxLabelSorter/Bad/"+imagem) for imagem in os.listdir('MedicineBoxLabelSorter/Bad')]
-
-# Processamento das imagens boas
-imagens_boas_processadas = []
-for imagem in imagens_boas:
-    # Aplique o processamento desejado à imagem aqui (redimensionamento, rotação, etc.)
-    imagem_processada = imagem  # Exemplo: sem processamento
-    imagens_boas_processadas.append(imagem_processada)
-
-# Processamento das imagens ruins
-imagens_ruins_processadas = []
-for imagem in imagens_ruins:
-    # Aplique o processamento desejado à imagem aqui (redimensionamento, rotação, etc.)
-    imagem_processada = imagem  # Exemplo: sem processamento
-    imagens_ruins_processadas.append(imagem_processada)
+imagens_boas = [cv2.imread("MedicineBoxLabelSorter/Generator/Good/"+imagem) for imagem in os.listdir('MedicineBoxLabelSorter/Generator/Good')]
+imagens_ruins = [cv2.imread("MedicineBoxLabelSorter/Generator/Bad/"+imagem) for imagem in os.listdir('MedicineBoxLabelSorter/Generator/Bad')]
 
 # Criar clipes de vídeo para imagens boas
-clips_boas = [ImageClip(img).set_duration(1) for img in imagens_boas_processadas]
+clips_boas = [(mp.ImageClip(img).set_duration(0.1)) for img in imagens_boas]
 
-# Criar clipes de vídeo para imagens ruins
-clips_ruins = [ImageClip(img).set_duration(1) for img in imagens_ruins_processadas]
-
-
-
-clips = [ImageClip(img).set_duration(1) for img in ["1.jpeg","2.jpeg","3.jpeg"]]
-
-videos= [CompositeVideoClip([
-    clip.fx(transfx.slide_out, duration=0.4, side="left")])
-    for clip in clips
-]
-video = concatenate_videoclips(videos, method="compose")
-video.write_videofile("output.mp4",
-    codec="libx264", audio_codec="aac",
-    preset="ultrafast", fps=24,
-    ffmpeg_params=[
-        "-vf", "pad=ceil(iw/2)*2:ceil(ih/2)*2",
-        "-pix_fmt", "yuv420p"
-    ]
-)
-
-
-# Criar o vídeo
-video = mp.ImageSequenceClip(frames, fps=fps)
-video = video.set_duration(duracao_video)
-
-# Salvar o vídeo
-video.write_videofile('esteira_simulacao.mp4')
+concat_clip = [mp.CompositeVideoClip([clip.fx( mp.transfx.slide_out, 0.1, 'left')]) for clip in clips_boas]
+concat_clip = mp.concatenate_videoclips(concat_clip)
+concat_clip.write_videofile("MedicineBoxLabelSorter/Generator/videos/Final.mp4", fps=24)
