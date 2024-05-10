f3052af749b8794f701340aba8c7fd75b0e1aa0d
Mateus Heck Soares <m_heck@outlook.com>
2024-05-10 03:15:43 -0300

    Organizing funcs into class

--- v4/util/CreateData.py
+++ v4/util/CreateData.py
@@ -1,6 +1,6 @@
 import string
 import os 
-from PIL import Image, ImageDraw, ImageFont
+from PIL import ImageDraw, ImageFont
 from PIL.Image import new
 import pandas as pd
 import numpy as np
@@ -8,81 +8,95 @@ import random
 import cv2 as cv
 import math 
 from skimage.util import random_noise
+import tqdm
 
-#Template text
-tmplt_text = 'F00/00      00:00\nV00/00\nA00A000'
-#Background Colors
-min_c_bg, max_c_bg = 105, 255
-#Fonts Colors
-min_c_font, max_c_font = 0, 50
-#Blur variant
-min_blur,max_blur = 1,5
-#Text distortion number of ondulations variant
-min_ond_numb, max_ond_numb = 2,7
-#Text distortion angle of ondulations variant
-min_angle, max_angle = 2,7
+class Create_dataset:
+    tmplt_text = 'F00/00      00:00\nV00/00\nA00A000' #Template text
 
-# Define texto para inserir na imagem
-def generate_random_text():
-    #randomiza os caracteres alfabéticos mantendom F:fabricação e V:validade
-    random_lote = ''.join(random.choice(string.ascii_uppercase) if char.isalpha() and char not in ["F", "V"] else char for char in tmplt_text)
-    #randomiza os números do texto (inclusive datas, mesmo que invalidas tem o intuíto de testar o maior número de combinações)
-    random_text = ''.join(random.choice(string.digits) if char.isdigit() else char for char in random_lote)
-    return random_text
+    #Ta aqui pq eu fiz diferente e é isso
+    def __init__(self):
+        pass #Muito legal isso ai
 
-def create_img():
-    bg_color = random.randint(min_c_bg, max_c_bg) #background color
-    font_color = random.randint(min_c_font, max_c_font) # font color
-    img = new(mode="RGB", size=(500,130), color=(bg_color, bg_color, bg_color)) #create canvas
-    font = ImageFont.truetype("v4/assets/DotFontMatrix.ttf", 50) #font path & size
-    text = generate_random_text() #call function
-    draw = ImageDraw.Draw(img) #specify where to write
-    draw.text((6,8), text, fill=(font_color, font_color, font_color), font=font) #write text
-    return img
+    #Automatizing process
+    def auto_generation(self):
+        self.bg_collor = random.randint(105, 255)  # background color
+        self.font_collor = random.randint(0, 50)  # font color
+        self.blur = random.randint(1, 5)  # Randomize blur
+        self.ond = random.randint(1, 7)  # Randomize n° ondulation
+        self.angle = random.randint(1, 7)  # Randomize ondulation size
+        self.noise = random.uniform(0, 0.7)  # Randomize noise mode
+        
+        #Calling functions in order to generate an unique image
+        text = self.generate_random_text()
+        img = self.create_img(text)
+        img_converted = self.convert_pil_cv(img)
+        img_blur = self.blur_img(img_converted)
+        img_distorted = self.acceptable_noise(img_blur)
+        img_edited = self.text_distortion(img_distorted)
+        return img_edited, text
+    
+    #Randomize Text
+    def generate_random_text(self):
+        #randomiza os caracteres alfabéticos mantendom F:fabricação e V:validade
+        random_lote = ''.join(random.choice(string.ascii_uppercase) if char.isalpha() and char not in ["F", "V"] else char for char in self.tmplt_text)
+        #randomiza os números do texto (inclusive datas, mesmo que invalidas tem o intuíto de testar o maior número de combinações)
+        random_text = ''.join(random.choice(string.digits) if char.isdigit() else char for char in random_lote)
+        return random_text
+    
+    #Create image using PIL
+    def create_img(self, text):
+        img = new(mode="RGB", size=(500,130), color=(self.bg_collor, self.bg_collor, self.bg_collor)) #create canvas
+        font = ImageFont.truetype("v4/assets/DotFontMatrix.ttf", 50) #font path & size
+        draw = ImageDraw.Draw(img) #specify where to write
+        draw.text((6,8), text, fill=(self.font_collor, self.font_collor, self.font_collor), font=font) #write text
+        return img
+    
+    #Convert PIL to CV
+    def convert_pil_cv(self, img_converted):
+        img = img_converted
+        img = img.convert("RGB") #Convert to RGB
+        cv_img = np.array(img)  #Convert to array
+        cv_img = cv_img[:, :, ::-1].copy() # Convert RGB to BGR
+        return cv_img
+    
+    #Add blur
+    def blur_img(self, img_converted):    
+        img = cv.blur(img_converted,(self.blur, self.blur)) #Aplly blur
+        return img
 
-def convert_pil_cv():
-    img = create_img()
-    img = img.convert("RGB") #Convert to RGB
-    cv_img = np.array(img)  #Convert to array
-    cv_img = cv_img[:, :, ::-1].copy() # Convert RGB to BGR
-    return cv_img
+         
+    #Add morfological distortion
+    def text_distortion(self, img_blur):
+        rows, cols, _ = img_blur.shape #Define image size
+        img_output = np.zeros(img_blur.shape, dtype=img_blur.dtype) #Define output
+        for n in range(rows):
+            for j in range(cols):
+                offset_x = 1 #Changing X axis pixel destination
+                offset_y = int(self.ond * math.cos(self.angle * math.pi * j / 300)) #Changing Y axis pixel destination
+                if n+offset_y < rows and j+offset_x < cols: #Check if the pixel is inside the original image
+                    img_output[n,j] = img_blur[(n+offset_y)%rows,(j+offset_x)%cols] #Move pixel to destination
+                else:
+                    img_output[n,j] = 0 #If destination is outside of the original image set collor to black
+        return img_output
 
-def blur_img():    
-    img = convert_pil_cv() 
-    blur = random.randint(min_blur,max_blur) #Randomize blur
-    img = cv.blur(img,(blur,blur)) #Aplly blur
-    return img
+    # Função para adicionar pontos aleatórios na imagem
+    def acceptable_noise(self, img_distorted):
+        gauss = np.random.normal(0,self.noise ,img_distorted.size) #Create an array with values between 0 and 0.5 with the same size of the image
+        gauss = gauss.reshape(img_distorted.shape[0],img_distorted.shape[1],img_distorted.shape[2]).astype('uint8') #Shape de array accordinly with n° col and row of the image
+        noise = img_distorted + img_distorted * gauss #Add noise
+        return noise
 
-def text_distortion():
-    img = convert_pil_cv()
-    ond = random.randint(min_ond_numb,max_ond_numb) #Randomize n° ondulation
-    angle = random.randint(min_angle,max_angle) #Randomize ondulation size
-    rows, cols, _ = img.shape #Define image size
-    img_output = np.zeros(img.shape, dtype=img.dtype) #Define output
-    for n in range(rows):
-        for j in range(cols):
-            offset_x = 1 #Changing X axis pixel destination
-            offset_y = int(ond * math.cos(angle * math.pi * j / 300)) #Changing Y axis pixel destination
-            if n+offset_y < rows and j+offset_x < cols: #Check if the pixel is inside the original image
-                img_output[n,j] = img[(n+offset_y)%rows,(j+offset_x)%cols] #Move pixel to destination
-            else:
-                img_output[n,j] = 0 #If destination is outside of the original image set color to black
-    return img_output
+labels = pd.DataFrame(columns=["id", "label"]) #Creating a Dataframe with the desirable columns
 
-# Função para adicionar pontos aleatórios na imagem
-def acceptable_noise():
-    img = convert_pil_cv()
-    mode = random.choice(["pepper", "gaussian"]) #Randomize noise mode
-    skimage_img = cv.cvtColor(img, cv.COLOR_BGR2RGB) #Convert img to be readable by skimage
-    noise_img = random_noise(skimage_img, mode=mode) #Apply noise mode
-    return noise_img
+num_img = 100 #N° of images to be generated
 
-# img = blur_img()
-# img = text_distortion()
-img = acceptable_noise()
-cv.imshow("teste", img) 
-blur_img()
-cv.waitKey(0)
+with tqdm.tqdm(total=num_img) as pbar:
+    for i in range(0,100):
+        dataset_creator = Create_dataset() #Calling constructor
+        img, text = dataset_creator.auto_generation() #Attributing the returns into img and text variables 
+        text = text.replace("\n", "").replace(" ", "") #Normalizing text to be readable
+        cv.imwrite(f"v4/assets/images/{i}.png",img) #Saving resulting image to path
+        labels.loc[len(labels.index)] = [i, text] #Writing into dataframe the respective image ID and Ground Thruth
+        pbar.update(1) #Updating progress bar
 
-
-
+labels.to_csv("v4/labels.csv", index = False) #Save dataframe to latter use
