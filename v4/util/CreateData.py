b099a90e98d2290efccf3b84aa78855781ce2747
Mateus Heck Soares <m_heck@outlook.com>
2024-05-09 22:23:28 -0300

    Func blur_img

--- v4/util/CreateData.py
+++ v4/util/CreateData.py
@@ -5,14 +5,14 @@ from PIL.Image import new
 import pandas as pd
 import numpy as np
 import random
-import cv2
+import cv2 as cv
 
-#Cores Background
-min_c_bg = 105
-max_c_bg = 255
-#Cores Fonte
-min_c_font = 0
-max_c_font = 50
+#Background Colors
+min_c_bg, max_c_bg = 105, 255
+#Fonts Colors
+min_c_font, max_c_font = 0, 50
+#Blur variant
+min_blur,max_blur = 1,5
 
 # Define texto para inserir na imagem
 def generate_random_text():
@@ -25,16 +25,38 @@ def generate_random_text():
     return random_text
 
 def create_img():
-    bg_color = random.randint(min_c_bg, max_c_bg)
-    font_color = random.randint(min_c_font, max_c_font)
-    img = new(mode="RGB", size=(500,130), color=(bg_color, bg_color, bg_color))
-    font = ImageFont.truetype("v4/assets/DotFontMatrix.ttf", 50)
-    text = generate_random_text() 
-    draw = ImageDraw.Draw(img)
-    draw.text((6,8), text, fill=(font_color, font_color, font_color), font=font)
-    img.show()
-create_img()
+    bg_color = random.randint(min_c_bg, max_c_bg) #background color
+    font_color = random.randint(min_c_font, max_c_font) # font color
+    img = new(mode="RGB", size=(500,130), color=(bg_color, bg_color, bg_color)) #create canvas
+    font = ImageFont.truetype("v4/assets/DotFontMatrix.ttf", 50) #font path & size
+    text = generate_random_text() #call function
+    draw = ImageDraw.Draw(img) #specify where to write
+    draw.text((6,8), text, fill=(font_color, font_color, font_color), font=font) #write text
+    # img.show() #show img
+    return img
 
-# def apply_skewing(random_text):
+def convert_pil_cv():
+    img = create_img()
+    img = img.convert("RGB")
+    cv_img = np.array(img)  
+    cv_img = cv_img[:, :, ::-1].copy() # Convert RGB to BGR
+    return cv_img
+
+def blur_img():    
+    img = convert_pil_cv()
+    blur = random.randint(min_blur,max_blur)
+    img = cv.blur(img,(blur,blur))
+    return img
+
+
+
+
+
+
+img = blur_img()
+cv.imshow("teste", img) 
+blur_img()
+cv.waitKey(0)
+
 
 
